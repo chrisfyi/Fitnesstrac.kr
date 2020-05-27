1b2a61e876588f8e5edaf8a2bd452e6e8fabdc71
Patricia Deal <gpatricia223@gmail.com>
2020-05-26 21:55:53 -0700

    finished debugging

--- db/seed.js
+++ db/seed.js
@@ -1,79 +1,100 @@
 const {
     client,
-    getAllUsers // new
+    // getAllUsers // new
   } = require('./index');
   
-  async function testDB() {
-    try {
-      client.connect();
-  
-      const users = await getAllUsers();
-      console.log(users);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      client.end();
-    }
-  }
-
-testDB();
 
 
 async function dropTables() {
     try {
+      console.log("Starting to drop tables...");
+
       await client.query(`
+      DROP TABLE IF EXISTS routine_activities;
+      DROP TABLE IF EXISTS routines;
       DROP TABLE IF EXISTS users;
-  
+      DROP TABLE IF EXISTS activities;
       `);
+
+      console.log("Finished dropping tables!")
     } catch (error) {
-      throw error; 
-    }
+      console.error("Error dropping tables!");
+      throw error;
+    } 
   }
   
  
   async function createTables() {
     try {
+      console.log("Starting to build tables...");
+
       await client.query(`
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL);
+        password varchar(255) NOT NULL
+      );
       CREATE TABLE activities (
-          id SERIAL PRIMARY KEY,
-          name varchar(255) UNIQUE NOT NULL,
-          description TEXT NOT NULL);
+        id SERIAL PRIMARY KEY,
+        name varchar(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL
+      );
       CREATE TABLE routines (
-          id SERIAL PRIMARY KEY,
-          "creatorId" INTEGER FOREIGN KEY,
-          public BOOLEAN DEFAULT false,
-          name varchar(255) UNIQUE NOT NULL,
-          goal TEXT NOT NULL);
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        public BOOLEAN DEFAULT false,
+        name varchar(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
+      );
       CREATE TABLE routine_activities (
-          id SERIAL PRIMARY KEY,
-          "routineId" INTEGER FOREIGN KEY,
-          "activityId" INTEGER FOREIGN KEY,
-          duration INTEGER,
-          count INTEGER);
-      )
+        id SERIAL PRIMARY KEY,
+        "routineId" INTEGER REFERENCES routines(id),
+        "activityId" INTEGER REFERENCES activities(id),
+        duration INTEGER,
+        count INTEGER
+      );
       `);
+      
+      console.log("Finished building tables!");
     } catch (error) {
-      throw error; 
+      console.error("Error building tables!");
+      throw error;
     }
   }
 
+    
+async function rebuildDB() {
+  try {
+    client.connect();
 
-  
-  async function rebuildDB() {
-    try {
-      client.connect();
-  
-      await dropTables();
-      await createTables();
-    } catch (error) {
-      console.error(error);
-    } finally {
-      client.end();
-    }
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
   }
-  
-  rebuildDB();
+}
+
+
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    // const users = await getAllUsers();
+    // console.log("getAllUsers:", users);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
+  }
+}
+
+testDB();
+
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
